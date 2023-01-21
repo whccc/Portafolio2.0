3bacdf67541e099ea914e310ef8dac3146739f9a
wilson castro <43712100+whccc@users.noreply.github.com>
2023-01-20 20:15:06 -0500

    ajuste links

--- src/components/CPCarousel/index.tsx
+++ src/components/CPCarousel/index.tsx
@@ -1,38 +1,47 @@
 import {
-    ContainerCarousel,
-    DivContainerOver,
-    DivContainerCarousel,
-    DivContainerCarouselItem,
-    ImgCarouselItem,
-    DivContainerOverData,
-    POverData,
-    H1OverData,
-    ImgOverData
-} from './styles'
+  ContainerCarousel,
+  DivContainerOver,
+  DivContainerCarousel,
+  DivContainerCarouselItem,
+  ImgCarouselItem,
+  DivContainerOverData,
+  POverData,
+  H1OverData,
+  ImgOverData,
+} from "./styles";
 const CPCarousel = () => {
-    return (
-        <ContainerCarousel>
-            <DivContainerCarousel controls={false} indicators={false}>
-                <DivContainerCarouselItem>
-                    <ImgCarouselItem
-                        src="/images/landing.jpg"
-                        alt="First slide"
-                    />
-                </DivContainerCarouselItem>
-            </DivContainerCarousel>
-            <DivContainerOver>
-                <DivContainerOverData className="animate__animated animate__bounce">
-                    <H1OverData>Wilson Herney Castro Cabrera</H1OverData>
-                    <POverData>Full Stack Javascript Developer</POverData>
+  const onGithubLink = () => {
+    window.open("https://github.com/whccc", "_blank");
+  };
 
-                    <POverData>
-                        <ImgOverData src="/images/github.png" />
-                        <ImgOverData src="/images/linkedin.png" />
-                    </POverData>
-                </DivContainerOverData>
-            </DivContainerOver>
-        </ContainerCarousel>
-    )
-}
+  const onLinkedin = () => {
+    window.open(
+      " https://www.linkedin.com/in/wilson-herney-castro-cabrera-73560a19a/",
+      "_blank"
+    );
+  };
 
-export default CPCarousel
+  return (
+    <ContainerCarousel>
+      <DivContainerCarousel controls={false} indicators={false}>
+        <DivContainerCarouselItem>
+          <ImgCarouselItem src="/images/landing.jpg" alt="First slide" />
+        </DivContainerCarouselItem>
+      </DivContainerCarousel>
+      <DivContainerOver>
+        <DivContainerOverData className="animate__animated animate__bounce">
+          <H1OverData>Wilson Herney Castro Cabrera</H1OverData>
+          <POverData>Full Stack Javascript Developer</POverData>
+
+          <POverData>
+            <ImgOverData src="/images/github.png" onClick={onGithubLink} />
+
+            <ImgOverData src="/images/linkedin.png" onClick={onLinkedin} />
+          </POverData>
+        </DivContainerOverData>
+      </DivContainerOver>
+    </ContainerCarousel>
+  );
+};
+
+export default CPCarousel;
--- src/components/CPCarousel/styles.ts
+++ src/components/CPCarousel/styles.ts
@@ -5,77 +5,78 @@ import { fadeInDown } from 'react-animations'
 const fadeInDownAnimation = keyframes`${fadeInDown}`
 
 export const ContainerCarousel = styled.div`
-    position: relative;
-    height: 300px;
+  position: relative;
+  height: 300px;
 
-    @media (min-width: 1024px) {
-        height: 600px;
-    }
+  @media (min-width: 1024px) {
+    height: 600px;
+  }
 `
 export const DivContainerCarousel = styled(Carousel)`
-    position: relative;
-    height: 300px;
-    @media (min-width: 1024px) {
-        height: 600px;
-    }
+  position: relative;
+  height: 300px;
+  @media (min-width: 1024px) {
+    height: 600px;
+  }
 `
 export const DivContainerCarouselItem = styled(Carousel.Item)`
-    height: 300px;
-    @media (min-width: 1024px) {
-        height: 600px;
-    }
+  height: 300px;
+  @media (min-width: 1024px) {
+    height: 600px;
+  }
 `
 
 export const ImgCarouselItem = styled.img`
-    width: 100%;
-    height: 300px;
-    @media (min-width: 1024px) {
-        height: 600px;
-    }
-    object-fit: cover;
+  width: 100%;
+  height: 300px;
+  @media (min-width: 1024px) {
+    height: 600px;
+  }
+  object-fit: cover;
 `
 export const DivContainerOver = styled.div`
-    position: absolute;
-    z-index: 1;
-    top: 0px;
-    background: rgb(0, 0, 0, 0.5);
+  position: absolute;
+  z-index: 1;
+  top: 0px;
+  background: rgb(0, 0, 0, 0.5);
 
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 export const DivContainerOverData = styled.div`
-    color: #fff;
-    text-align: center;
-    animation: 1s ${fadeInDownAnimation};
+  color: #fff;
+  text-align: center;
+  animation: 1s ${fadeInDownAnimation};
 `
 export const H1OverData = styled.h1`
-    font-weight: 900;
-    font-size: 25px;
-    margin: 0px;
-    @media (min-width: 1024px) {
-        font-size: 50px;
-    }
-    text-shadow: 1px 1px 2px #000;
+  font-weight: 900;
+  font-size: 25px;
+  margin: 0px;
+  @media (min-width: 1024px) {
+    font-size: 50px;
+  }
+  text-shadow: 1px 1px 2px #000;
 `
 export const POverData = styled.p`
-    margin: 0px;
-    font-size: 28px;
-    font-weight: 900;
+  margin: 0px;
+  font-size: 28px;
+  font-weight: 900;
 
-    text-shadow: 1px 1px 2px #000;
+  text-shadow: 1px 1px 2px #000;
 `
 
 export const ImgOverData = styled.img`
-    width: 50px;
-    background-color: #fff;
-    border: 1px solid #fff;
-    border-radius: 100%;
-    margin: 5px;
-    @media (min-width: 1024px) {
-        width: 70px;
-    }
+  width: 50px;
+  background-color: #fff;
+  border: 1px solid #fff;
+  border-radius: 100%;
+  margin: 5px;
+  cursor: pointer;
+  @media (min-width: 1024px) {
+    width: 70px;
+  }
 `
